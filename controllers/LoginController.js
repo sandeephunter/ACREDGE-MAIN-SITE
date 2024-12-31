e8de31902744575c82a3f8729823c3e54f9e3654
SANDEEP GUPTA <sandeepguptax2003@gmail.com>
2024-12-31 13:08:09 +0530

    back to same cookies

--- controllers/LoginController.js
+++ controllers/LoginController.js
@@ -54,7 +54,7 @@ exports.verifyFirebaseToken = async (req, res) => {
     res.cookie('token', token, {
       httpOnly: true,
       secure: true,
-      sameSite: none,
+      sameSite: 'none',
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
